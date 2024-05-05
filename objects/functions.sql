a4d08027475e5ef0c06e5dc8d10aa1e69771f284
NicolasAlarconS <40845814+NicolasAlarconS@users.noreply.github.com>
2024-05-05 18:06:28 -0400

    Update functions.sql

--- objects/functions.sql
+++ objects/functions.sql
@@ -1,50 +1,53 @@
-USE Projecto_Nicolas;
--- Funcion de calculo total de ventas por cliente determinado
+-- Usar la base de datos creada
+USE DB_NicolasAlarcon;
+
+-- Función para obtener el nombre completo del empleado según su ID
 DELIMITER //
 
-CREATE FUNCTION calcular_total_ventas_cliente(cliente_id VARCHAR(50)) 
-RETURNS DECIMAL(18, 2) 
-READS SQL DATA
-BEGIN
-    DECLARE total DECIMAL(18, 2);
-    SELECT SUM(v.Venta_Neta) INTO total 
-		FROM ventas v
-		JOIN pedidos p ON v.ID_Pedido = p.ID_Pedido
-		WHERE p.ID_Cliente = cliente_id;
-    RETURN total;
-END//
-
-DELIMITER ;
-
-DELIMITER //
-
--- Funcion de obtencion nombre completo segun cliente
-CREATE FUNCTION obtener_nombre_completo_cliente(cliente_id VARCHAR(50)) 
-RETURNS VARCHAR(100) 
-READS SQL DATA
+CREATE FUNCTION obtener_nombre_completo_empleado(empleado_id VARCHAR(50))
+RETURNS VARCHAR(100) DETERMINISTIC
 BEGIN
     DECLARE nombre_completo VARCHAR(100);
     SELECT CONCAT(Nombre, ' ', Apellido) INTO nombre_completo
-    FROM clientes
-    WHERE ID_Cliente = cliente_id;
+    FROM Empleados
+    WHERE IDEmpleado = empleado_id;
     RETURN nombre_completo;
 END//
 
+//
+
 DELIMITER ;
 
+-- Función para calcular el salario anual de un empleado
 DELIMITER //
 
--- Funcion de obtencion de canal de venta segun pedido
-CREATE FUNCTION obtener_canal_segun_pedido(pedido_id VARCHAR(50)) 
-RETURNS VARCHAR(50) 
-READS SQL DATA
+CREATE FUNCTION calcular_salario_anual_empleado(empleado_id VARCHAR(50))
+RETURNS DECIMAL(10, 2) DETERMINISTIC
 BEGIN
-    DECLARE nombre_canal VARCHAR(50);
-    SELECT c.Nombre INTO nombre_canal
-    FROM pedidos p
-    JOIN canales c ON p.ID_Canal = c.ID_Canal
-    WHERE p.ID_Pedido = pedido_id;
-    RETURN nombre_canal;
+    DECLARE salario_anual DECIMAL(10, 2);
+    SELECT Salario * 12 INTO salario_anual
+    FROM Empleados
+    WHERE IDEmpleado = empleado_id;
+    RETURN salario_anual;
 END//
+
+//
+
+DELIMITER ;
+
+-- Función para obtener el departamento de un empleado según su ID
+DELIMITER //
+
+CREATE FUNCTION obtener_departamento_empleado(empleado_id VARCHAR(50))
+RETURNS VARCHAR(50) DETERMINISTIC
+BEGIN
+    DECLARE departamento VARCHAR(50);
+    SELECT Nombre INTO departamento
+    FROM Departamentos
+    WHERE IDDepartamento = (SELECT IDDepartamento FROM Empleados WHERE IDEmpleado = empleado_id);
+    RETURN departamento;
+END//
+
+//
 
 DELIMITER ;
