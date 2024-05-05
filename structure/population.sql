477ff71e0cbc2606723b205bd42d7c0709e944e8
NicolasAlarconS <40845814+NicolasAlarconS@users.noreply.github.com>
2024-05-05 13:38:11 -0400

    Update population.sql

--- structure/population.sql
+++ structure/population.sql
@@ -3,44 +3,48 @@ USE Projecto_Nicolas;
 SET GLOBAL local_infile = true;
 
 -- Cargar datos desde archivos CSV
--- Cargar datos tabla paises
-LOAD DATA  LOCAL INFILE  '/structure/data_csv/paises.csv'
-INTO TABLE paises
-FIELDS TERMINATED BY ',' ENCLOSED BY '"'
+
+LOAD DATA INFILE '/structure/data_csv/paises.csv' INTO TABLE Paises
+FIELDS TERMINATED BY '%' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 IGNORE 1 LINES;
 
--- Cargar datos tabla productos
-LOAD DATA  LOCAL INFILE  '/structure/data_csv/productos.csv'
-INTO TABLE productos
-FIELDS TERMINATED BY ',' ENCLOSED BY '"'
+LOAD DATA INFILE '/structure/data_csv/paises.csv' INTO TABLE Clientes
+FIELDS TERMINATED BY '%'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
+
+LOAD DATA INFILE '/structure/data_csv/paises.csv' INTO TABLE Productos
+FIELDS TERMINATED BY '%' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 IGNORE 1 LINES;
 
--- Cargar datos tabla canales
-LOAD DATA  LOCAL INFILE  '/structure/data_csv/canales.csv'
-INTO TABLE canales
-FIELDS TERMINATED BY ',' ENCLOSED BY '"'
+LOAD DATA INFILE '/structure/data_csv/paises.csv' INTO TABLE Canales
+FIELDS TERMINATED BY '%' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 IGNORE 1 LINES;
 
--- Cargar datos tabla clientes
-LOAD DATA  LOCAL INFILE  '/structure/data_csv/clientes.csv'
-INTO TABLE clientes
-FIELDS TERMINATED BY ',' ENCLOSED BY '"'
+LOAD DATA INFILE '/structure/data_csv/paises.csv' INTO TABLE Departamentos
+FIELDS TERMINATED BY '%' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 IGNORE 1 LINES;
 
--- Cargar datos tabla pedidos
-LOAD DATA  LOCAL INFILE  '/structure/data_csv/pedidos.csv'
-INTO TABLE pedidos
-FIELDS TERMINATED BY ','
+LOAD DATA INFILE '/structure/data_csv/paises.csv' INTO TABLE Empleados
+FIELDS TERMINATED BY '%' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 IGNORE 1 LINES;
 
--- Cargar datos tabla ventas
-LOAD DATA  LOCAL INFILE  '/structure/data_csv/ventas.csv'
-INTO TABLE ventas
-FIELDS TERMINATED BY ',' ENCLOSED BY '"'
+LOAD DATA INFILE '/structure/data_csv/paises.csv' INTO TABLE Ventas
+FIELDS TERMINATED BY '%' ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
+
+LOAD DATA INFILE '/structure/data_csv/paises.csv' INTO TABLE Pedidos
+FIELDS TERMINATED BY '%' ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
+
+LOAD DATA INFILE '/structure/data_csv/paises.csv' INTO TABLE Proveedores
+FIELDS TERMINATED BY '%' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 IGNORE 1 LINES;
